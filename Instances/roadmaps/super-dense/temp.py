c631c81c656f6f6ed152e95eef2610a49f7e24d0
andy li <1953812182@qq.com>
2023-04-17 17:16:13 +1000

    ready for exp

---
+++ Instances/roadmaps/super-dense/temp.py
@@ -0,0 +1,28 @@
+with open("map.graph",'r') as f:
+    n=int(f.readline())
+    nodes=[]
+    for _ in range(n):
+        temp=f.readline().strip().split(' ')
+        temp=tuple(map(lambda x:float(x),temp))
+        print(temp)
+        nodes.append(temp)
+    '''
+    n=int(f.readline())
+    edges=[]
+    for l in range(n):
+        l=f.readline().split(' ')
+        l=list(map(int,l))
+        if l[0]==85: l[0]=120
+        if l[1]==85: l[1]=120
+        if l in edges:
+            print(l)
+        else:
+            edges.append(l)
+
+
+with open("new_map.graph","w") as f:
+    print(len(nodes),file=f)
+    [print(i,end='',file=f) for i in nodes]
+    print(len(edges),file=f)
+    [print(*i,file=f) for i in edges]
+'''
